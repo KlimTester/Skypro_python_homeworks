3af294b93d12964f892363d52c64beea6f08cd27
KlimTester <klimov888ily@live.com>
2024-02-28 15:40:04 +0100

    "add first homework"

---
+++ Lesson1/Lesson_1_task_6.py
@@ -0,0 +1,32 @@
+def print_0():
+    print(0, end="")
+
+def print_1():
+    print(1, end="")
+
+def print_2():
+    print(2, end="")
+
+def print_3():
+    print(3, end="")
+
+def print_4():
+    print(4, end="")
+
+def print_5():
+    print(5, end="")
+
+def print_6():
+    print(6, end="")
+
+def print_7():
+    print(7, end="")
+
+def print_8():
+    print(8, end="")
+
+def print_9():
+    print(9, end="")
+
+print_8(), print_8(), print_0(), print_0(), print_5(), print_5(), print_5(), print_3(), print_5(), print_3(), print_5()
+
