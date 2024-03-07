05ba8ab47d5ea4c646230646cd628084a8688ab8
KlimTester <klimov888ily@live.com>
2024-03-07 22:45:18 +0100

    add third homework

--- Lesson3/Lesson_3_task_2.py
+++ Lesson3/Lesson_3_task_2.py
@@ -2,13 +2,11 @@ from smartphone import Smartphone
 
 catalog = []
 
-
 catalog.append(Smartphone("Samsung", "Galaxy S20", "+79123456789"))
 catalog.append(Smartphone("Apple", "iPhone 12", "+79234567890"))
 catalog.append(Smartphone("Xiaomi", "Redmi Note 10", "+79345678901"))
 catalog.append(Smartphone("OnePlus", "8T", "+79456789012"))
 catalog.append(Smartphone("Google", "Pixel 5", "+79567890123"))
-
 
 for phone in catalog:
     print(f"{phone.brand} - {phone.model}. {phone.phone_number}")
