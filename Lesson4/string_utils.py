6e7c4d8d7cd449c731629014d60649c50fb09be3
KlimTester <klimov888ily@live.com>
2024-03-15 15:34:32 +0100

    git commit -m "add my  4 homework "

---
+++ Lesson4/string_utils.py
@@ -0,0 +1,18 @@
+class StringUtils:
+    @staticmethod
+    def is_palindrome(s: str) -> bool:
+        
+        return s == s[::-1]
+    
+    @staticmethod
+    def reverse_string(s: str) -> str:
+       
+        return s[::-1]
+
+
+
+
+
+
+
+
---
+++ Lesson4/test_string_utils.py
@@ -0,0 +1,47 @@
+import pytest
+from string_utils import StringUtils
+
+class TestStringUtils:
+    # Positive scenarios
+    def test_is_palindrome_positive(self):
+        assert StringUtils.is_palindrome("racecar") == True
+    
+    def test_is_palindrome_negative(self):
+        assert StringUtils.is_palindrome("Тачка") == False
+    
+    def test_reverse_string_positive(self):
+        assert StringUtils.reverse_string("Тачка") == "акчаТ"
+    
+    def test_reverse_string_numbers(self):
+        assert StringUtils.reverse_string("123456789") == "987654321"
+    
+    # Negative scenarios
+    def test_is_palindrome_empty_string(self):
+        assert StringUtils.is_palindrome("") == True
+    
+    def test_is_palindrome_string_with_spaces(self):
+        assert StringUtils.is_palindrome("  ") == True
+    
+    def test_is_palindrome_none(self):
+        with pytest.raises(TypeError):
+            StringUtils.is_palindrome(None)
+    
+    def test_reverse_string_empty_string(self):
+        assert StringUtils.reverse_string("") == ""
+    
+    def test_reverse_string_string_with_spaces(self):
+        assert StringUtils.reverse_string("  ") == "  "
+    
+    def test_reverse_string_none(self):
+        with pytest.raises(TypeError):
+            StringUtils.reverse_string(None)
+
+
+
+
+
+
+
+
+
+
